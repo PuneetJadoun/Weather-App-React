e5d3bee7ce3ea797208f0ca0cef2218335eabaa3
Puneet kumar Singh <punitku624@gmail.com>
2025-07-14 23:33:17 +0530

    Starting Project with Baisc Setup

---
+++ weather-app/src/SearchBox.jsx
@@ -0,0 +1,11 @@
+import TextField from '@mui/material/TextField';
+export default function SearchBox() {
+  return (
+    <>
+    <h3>Search for the Weather</h3>
+    <form>
+         <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+    </form>
+    </>
+  );
+}
