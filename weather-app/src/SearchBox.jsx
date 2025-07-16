5a8425c9544633e01c338061830fa4dc1168dfa2
Puneet kumar Singh <punitku624@gmail.com>
2025-07-17 01:04:19 +0530

    Use Api key to fetch data

--- weather-app/src/SearchBox.jsx
+++ weather-app/src/SearchBox.jsx
@@ -4,18 +4,43 @@ import './SearchBox.css'
 import { useState } from 'react';
 
 export default function SearchBox() {
+  const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+  const API_KEY = "2ec7a06932656d9595db68551e28118f";
+
+  let getWeatherInfo = async () =>{
+    let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+    let jsonResponse = await response.json();
+    console.log(jsonResponse);
+
+    let result = {
+      temp: jsonResponse.main.temp,
+      tempMin: jsonResponse.main.temp_min,
+      tempMax: jsonResponse.main.temp_max,
+      humidity: jsonResponse.main.temp.humidity,
+      feelsLike: jsonResponse.main.feels_like,
+      weather: jsonResponse.weather[0].description,
+    };
+    console.log(result);
+  }
+
   let [city,setCity] = useState("");
 
   let handleChange = (evnt) =>{
     setCity(evnt.target.value);
   }
 
+  let handleSubmit = (evnt) =>{
+    evnt.preventDefault();
+    console.log(city);
+    setCity("");
+    getWeatherInfo();
+  }
 
 
   return (
     <div className='SearchBox'>
     <h3>Search for the Weather</h3>
-    <form>
+    <form onSubmit={handleSubmit}>
         <TextField id="city" label="City Name" variant="outlined" required value={city} onChange={handleChange} />
         <br />
         <br />
